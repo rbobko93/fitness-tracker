d67a33edbb9cfd67a7638e064ade34192cc53a22
rbobko <rbobko70@gmail.com>
2019-08-14 18:26:10 +0200

    Adding login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EmailValidator, FormControl, FormGroup, RequiredValidator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('',
+        {
+          validators: [Validators.email,
+            Validators.required]
+        }),
+      password: new FormControl('', {validators: Validators.required})
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
 }
